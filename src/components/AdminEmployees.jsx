2b648933f636549a60a8860ee16fbcdb7c44f84b
scashby <38795660+scashby@users.noreply.github.com>
2025-05-05 21:57:59 +0000

    adminemployees update 2

--- src/components/AdminEmployees.jsx
+++ src/components/AdminEmployees.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../supabaseClient.js';
+import { supabase } from '../supabaseClient';
 
 const AdminEmployees = () => {
   const [employees, setEmployees] = useState([]);
