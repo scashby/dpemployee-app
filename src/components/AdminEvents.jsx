a202a2a8ac7994c2c581fefb400cfe7ca2d5a0fa
scashby <38795660+scashby@users.noreply.github.com>
2025-05-07 04:00:20 +0000

    dpcss imported

--- src/components/AdminEvents.jsx
+++ src/components/AdminEvents.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabase/supabaseClient';
+import '../styles/devils-purse.css';
 
 const AdminEvents = () => {
   const [events, setEvents] = useState([]);
