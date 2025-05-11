55b2d2bfc1944cafa55fc6ac986b6040d8ce4796
scashby <38795660+scashby@users.noreply.github.com>
2025-05-11 04:18:48 +0000

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  optimizeDeps: {
+    include: ['pdf-lib']
+  },
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  }
 });
